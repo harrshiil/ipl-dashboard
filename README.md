f78d9b8e6dfc40d1d59a4f84f6e2891110b48a3b
Harshil <harshil.parmar9087@gmail.com>
2021-08-10 19:08:35 +0530

    New Changes

--- README.md
+++ README.md
@@ -17,3 +17,16 @@ https://spring.io/guides/gs/batch-processing/
 #### GitHub Repo: https://github.com/koushikkothagal/ipl-dashboard
 
 Create Frontend via - npx create-react-app frontend
+
+#
+Earlier frontend was running on 3000 and backend was running on 8080,
+
+but now we want to run both system on 8080. so we modifed a bulid script in react package.json
+that after completing build process all the files should copy to main/resources/public
+
+problem solved. another prob is when we start app from root. react knows where to route
+but if we refresh any page in between react does not know.
+
+one way to fix the problem is hash router. by this way you get additional hash as below
+
+http://localhost:8080/#/teams -> after this it will route every request to index.html 
--- src/frontend/src/pages/HomePage.js
+++ src/frontend/src/pages/HomePage.js
@@ -1,18 +1,16 @@
 import './HomePage.scss';
 import { React, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import { TeamTile } from '../components/TeamTile';
 
 
 export const HomePage = () => {
 
     const [teams, setTeam] = useState([]); // teamObject will set by setTeam() method // initial init
-    const { teamName } = useParams();
 
     useEffect(
         () => {
             const fetchAllTeams = async () => {
-                const response = await fetch(`http://localhost:8080/teams`);
+                const response = await fetch(`${process.env.REACT_APP_API_ROOT_URL}/teams`);
                 const data = await response.json();
                 setTeam(data);
             };
@@ -25,7 +23,7 @@ export const HomePage = () => {
             <div className="header-section"><h1 className="app-name">IPL Dashboard</h1></div>
 
             <div className="team-grid">
-                {teams.map(team => <TeamTile teamName={team.teamName}/>)}
+                {teams.map(team => <TeamTile key={team.id} teamName={team.teamName}/>)}
             </div>
         </div>
     );
